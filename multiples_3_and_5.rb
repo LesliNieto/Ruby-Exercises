27f1aa211a2633ccd43c8e1d026bedad280e16d0
Lesli Nieto <lesli.nieto@koombea.com>
2018-10-02 14:51:23 -0500

    error mio

--- multiples_3_and_5.rb
+++ multiples_3_and_5.rb
@@ -5,8 +5,7 @@
 def multiples_3_5(m3, m5, max)
   (1...max).select {|number| (number % m3 ).zero? || (number % m5).zero? }.inject(:+)
 end
-
-p multiples_3_5(3, 5, 1000)
+p multiples_3_5(3, 5, 10)
 
 
 
