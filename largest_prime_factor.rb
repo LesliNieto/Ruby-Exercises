67aaabeb5799a60c1c18efd9039558bbfc28d301
Lesli Nieto <lesli.nieto@koombea.com>
2018-10-04 15:12:42 -0500

    Prime class

--- largest_prime_factor.rb
+++ largest_prime_factor.rb
@@ -1,16 +1,10 @@
 #The prime factors of 13195 are 5, 7, 13 and 29.
 #What is the largest prime factor of the number 600851475143 ?
 
+require 'prime'
+
 def prime_factor(num)
-  factor = 2
-  until num == 1
-    if (num % factor).zero?
-      num = num.div(factor)
-    else
-      factor += 1
-    end
-  end
-  factor
+  Prime.prime_division(num).flatten.max
 end
 
 puts prime_factor(600851475143)
